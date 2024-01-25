81c87de6b5a52604a561a77a2acddc35fe10cd0c
Lenzo, Colin James <cjlenzo@iu.edu>
2024-01-25 16:16:45 -0500

    Initial commit

---
+++ src/test/java/c322spring2024homework2/work/WorkApplicationTests.java
@@ -0,0 +1,13 @@
+package c322spring2024homework2.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
