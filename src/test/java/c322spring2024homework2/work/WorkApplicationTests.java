53f519717eeb3d8932ac9326f91b04c720a44269
Lenzo, Colin James <cjlenzo@iu.edu>
2024-04-22 22:49:39 -0400

    Changes for Homework 9 Part 2

--- src/test/java/c322spring2024homework2/work/WorkApplicationTests.java
+++ src/test/java/c322spring2024homework2/work/WorkApplicationTests.java
@@ -3,10 +3,9 @@ package c322spring2024homework2.work;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class WorkApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
